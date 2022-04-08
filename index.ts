fb0b0e30091f6511968d14faa1452e1d4a6cddf0
Aytixel <aytixel@gmail.com>
2022-04-08 20:01:52 +0200

    Add cursor movement

---
+++ index.ts
@@ -0,0 +1,6 @@
+import {
+  GridFSBucket,
+  MongoClient,
+} from "https://deno.land/x/mongo@v0.29.3/mod.ts";
+
+const client = new MongoClient();
---
+++ upload.ts
@@ -0,0 +1,46 @@
+import {
+  GridFSBucket,
+  MongoClient,
+} from "https://deno.land/x/mongo@v0.29.3/mod.ts";
+
+const client = new MongoClient();
+
+await client.connect({
+  db: "image",
+  tls: true,
+  servers: [
+    {
+      host: "cluster0-shard-00-00.s8vpt.mongodb.net",
+      port: 27017,
+    },
+    {
+      host: "cluster0-shard-00-01.s8vpt.mongodb.net",
+      port: 27017,
+    },
+    {
+      host: "cluster0-shard-00-02.s8vpt.mongodb.net",
+      port: 27017,
+    },
+  ],
+  credential: {
+    username: "user",
+    password: "qRUbZmd0CC4oxE9S",
+    db: "image",
+    mechanism: "SCRAM-SHA-1",
+  },
+});
+
+const image_database = client.database("image");
+
+const bucket = new GridFSBucket(image_database);
+
+await bucket.delete(
+  (await bucket.find({ filename: "current" }).toArray())[0]._id,
+);
+
+const upstream = await bucket.openUploadStream("current");
+const writer = await upstream.getWriter();
+
+writer.write(await Deno.readFile("original.webp"));
+
+await writer.close();
